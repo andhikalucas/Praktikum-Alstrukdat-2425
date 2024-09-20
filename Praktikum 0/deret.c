f480a434c5c504fcfb869f1309a7c1525918b0ea
lucasnicoa <164333795+andhikalucas@users.noreply.github.com>
2024-09-20 14:54:23 +0700

    soal 4 -revisi

--- Praktikum 0/deret.c
+++ Praktikum 0/deret.c
@@ -1,22 +1,32 @@
 // Nicholas Andhika Lucas 13523014
-// Program menerima bil int a,b dan mengoperasikan bilangan a hingga <= b
+// Program menerima bil int a,b dan menampilkan operasi bilangan a hingga <= b
 #include <stdio.h>
 int main(){
 int a, b;
 scanf("%d %d", &a, &b);
-if ((1 <= a) && (b <= 1000) && (a < b)){
-    printf("%d", a);
-    while(a < b){
-        if (a % 2 == 0){
-            a = a * 2;
-        }
-        else if (a % 2 == 1){
-            a = a + 1;
-        }
-        if (a <= b){
-        printf(" %d", a);}
+printf("%d", a);
+while (a <= b){
+    if (a % 2 == 0){
+        a *= 2;
     }
-printf("\n");
+    else {a++;}
+    if (a <= b){
+    printf(" %d", a);}
 }
+printf("\n");
 return 0;
 }
+
+/* ALTERNATIF Cara Print
+while (a <= b) {
+
+        printf("%d", a);
+
+        if (a%2 == 0) a*=2;
+
+        else a++;
+
+        if (a <= b) printf(" ");
+
+    }
+*/
