01878f2842b929fd28bf403f6ce3e5f6d8627cb9
lucasnicoa <164333795+andhikalucas@users.noreply.github.com>
2024-09-20 14:57:08 +0700

    soal 2 -alternatif jawaban

--- Praktikum 0/segitiga.c
+++ Praktikum 0/segitiga.c
@@ -25,3 +25,20 @@ int main(){
     }
     return 0;
 }
+
+/* Alternatif Jawaban
+int main(){
+    int n;
+    scanf("%d", &n);
+    int i;
+    for (i = n; i > 0; i-=1){
+        int j;
+        for (j = n - i; j > 0; j--) printf(" ");
+        for (j = 0; j < i; j+= 1) printf("*");
+        for (j = 1; j < i; j+= 1) printf("*");
+        printf("\n");
+    }
+    return 0;
+
+}
+*/
