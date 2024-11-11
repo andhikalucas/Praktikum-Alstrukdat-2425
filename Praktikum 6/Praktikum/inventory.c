52b9887882af9da2919502ef626f7537295c8d8b
lucasnicoa <164333795+andhikalucas@users.noreply.github.com>
2024-11-11 13:11:44 +0700

    fix ans 1

--- Praktikum 6/Praktikum/inventory.c
+++ Praktikum 6/Praktikum/inventory.c
@@ -1,65 +1,69 @@
-#include "stack.h"
 #include <stdio.h>
+#include "boolean.h"
+#include "stack.h"
+
+// HINT: Gunakan (c - '0') untuk mengubah character ke angka
 
 int main(){
     int n;
     scanf("%d", &n);
-    char cc[n];
-    scanf("%s", cc);
-
-    Stack s,temp;
+    char ch;
+    Stack s;
     CreateEmpty(&s);
-    CreateEmpty(&temp);
 
     for (int i = 0; i < n; i++){
-        // Bil. Bulat
-        if  (cc[i] >= 48 && cc[i] <= 57){
-            Push(&s, cc[i] - 48);
-        }
+        scanf(" %c", &ch);
+
         // Not Bil. Bulat
-        else{ 
-            if (cc[i] == '+'){
-                int val1, val2;
-                // Get last 2 numbers from stack
-                Pop(&s, &val1);
-                Pop(&s, &val2);
-                // Return last 2 numbers to stack (aka naikin index topnya balik ajah)
-                Top(s) += 2;
-                // Get sum and push sum to stack
-                int sum = val1 + val2;
-                Push(&s, sum);
-            }
+        if (ch == '+'){
+            int val1, val2;
+            // Get last 2 numbers from stack
+            Pop(&s, &val1);
+            Pop(&s, &val2);
+            // Return last 2 numbers to stack (aka naikin index topnya balik ajah)
+            Top(s) += 2;
+            // Get sum and push sum to stack
+            int sum = val1 + val2;
+            Push(&s, sum);
+        }
 
-            if (cc[i] == 'X'){
-                int apalah;
-                Pop(&s, &apalah);
-            }
+        else if (ch == 'X'){
+            int apalah;
+            Pop(&s, &apalah);
+        }
 
-            if (cc[i] == 'S'){
-                int val;
-                int sum = 0;
+        else if (ch == 'S'){
+            int val;
+            int sum = 0;
+            while(!IsEmpty(s)){
+                Pop(&s, &val);
+                sum += val;
+            }
+            // EOP : S empty
+            Push(&s, sum);
+        }
+
+        else if (ch == 'A'){
+            int val;
+            int sum = 0;
+            int count = 0;
+            if (!IsEmpty(s)){
                 while(!IsEmpty(s)){
-                    Pop(&s, &val);
-                    sum += val;
-                }
-                // EOP : S empty
-                Push(&s, sum);
-            }
-
-            if (cc[i] == 'A'){
-                int val;
-                int sum = 0;
-                int count = 0;
-                while(!IsEmpty(s)){
-                    Pop(&s, &val);
-                    sum += val;
-                    count++;
+                Pop(&s, &val);
+                sum += val;
+                count++;
                 }
                 // EOP : S empty
                 Push(&s, sum/count);
             }
         }
-    }
+
+        // Bil. Bulat
+        else{  //(ch >= 48 && ch <= 57)
+            if (ch >= 48 && ch <= 57) Push(&s, ch - '0');
+        }
+        }
+    
     // Find total of stack
     int sum = 0;
     int val;
@@ -68,6 +72,5 @@ int main(){
         sum += val;
     }
     printf("%d\n", sum);
-    // 0 = 48
-    // 1 = 49
+    return 0;
 }
